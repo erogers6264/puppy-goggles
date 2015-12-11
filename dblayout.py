ea849b9c43fd0dea47c672d4b1baab2842484f21
Ethan Rogers <erogers6264@gmail.com>
2015-12-11 11:35:25 -0700

    Fill in class attributes to be mapped

--- dblayout.py
+++ dblayout.py
@@ -20,24 +20,42 @@ Base = declarative_base()
 # Time to write the classes which will be mapped
 class Shelter(Base):
 	"""docstring for Shelter"""
+	__tablename__ = 'shelter'
 	name = Column(String(80), nullable = False)
 	address = Column(String)
 	city = Column(String)
 	state = Column(String)
 	zipCode = Column(Integer)
 	website = Column(String) 
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key = True)
 
-	def __init__(self, arg):
-		super(Shelter, self).__init__()
-		self.arg = arg
+	def __init__(self, name, address, city, 
+				 state, zipCode, website, id):
+		self.name = name
+		self.address = address
+		self.city = city
+		self.state = state
+		self.zipCode = zipCode
+		self.website = website
+		self.id = id
 
 	def __repr__(self):
-		return "<User('%s','%s', '%s')>" % (self.name, self.address, self.city, self.state, self.zipCode, self.website, self.id, 
+		return "<User('%s','%s', '%s')>" % (self.name, self.address, self.city,
+											self.state, self.zipCode,
+											self.website, self.id)
 
 
 class Puppy(Base):
 	"""docstring for Puppy"""
+
+	__tablename__ = 'puppy'
+
+	name = Column(String(80), nullable = False)
+	dateOfBirth = Column() # Maybe look up a date data type
+	gender = Column()
+	weight = Column()
+	shelter_id = Column()
+
 	def __init__(self, arg):
 		super(Puppy, self).__init__()
 		self.arg = arg
