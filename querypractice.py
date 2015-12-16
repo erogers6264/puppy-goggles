8b4fd1f7fa3d66bd78beba011ef8942eaf8afe23
Ethan Rogers <erogers6264@gmail.com>
2015-12-15 23:08:48 -0700

    Organize functions for the query exercises

--- querypractice.py
+++ querypractice.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
@@ -8,14 +9,36 @@ from datetime import timedelta
 engine = create_engine('sqlite:///puppyshelter.db')
 
 Base.metadata.bind = engine
- 
+
 DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-sixMonths = timedelta(days=180)
-print(session.query(Puppy).all())
-lessThan6Months = session.query(Puppy).filter(timedelta(\
-								Puppy.dateOfBirth) <= sixMonths)
-for i in lessThan6Months:
-	print i.name, i.dateOfBirth
+
+# Show all puppies
+def showPuppies():
+	print(session.query(Puppy).all())
+
+# Show all puppiers less than 6 months old ordered by the 
+# yougest first
+def lessSixMonths():
+	today = datetime.date.today()
+	sixMonths = timedelta(days=180)
+	sixMonthsAgo = today - sixMonths
+
+	lessThan6Months = session.query(Puppy).filter(sixMonthsAgo < Puppy.dateOfBirth)\
+					  .order_by(Puppy.dateOfBirth.desc())
+	for i in lessThan6Months:
+	 	print i.name, i.dateOfBirth
+
+# Show puppies by ascending weight
+def weightAscending():
+	weightAscending = session.query(Puppy).order_by(Puppy.weight)
+	for i in weightAscending:
+		print i.name, i.weight
+
+# Show all puppies grouped by the shelter they are staying in
+def byShelter():
+	byShelter = session.query(Puppy).join("shelter").order_by(Shelter.name)
+	for pup in byShelter:
+		print pup.name, pup.shelter.name
