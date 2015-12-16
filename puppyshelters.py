2ab292ac9918ab5ac98f607cbd6c201c7a2f54c2
Ethan Rogers <erogers6264@gmail.com>
2015-12-15 23:10:20 -0700

    Add initial module to check in puppies

---
+++ puppyshelters.py
@@ -0,0 +1,30 @@
+#
+# This module provides functions to work with the database
+#
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+ 
+from dblayout import Base, Shelter, Puppy, Adopter, Profile 
+
+engine = create_engine('sqlite:///puppyshelter.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+
+# Create a function to check a puppy into a shelter, if the shelter is at
+# capacity, prompt the user to try a different shelter. If you’re feeling
+# extra ambitious write an algorithm that will find another shelter in the
+# same area or with a higher vacancy. If all shelters are full, prompt the
+# user that a new shelter needs to be opened.
+
+# Feeling Extra Udacious?
+# Write a load-balancing algorithm that can transfer puppies evenly throughout
+# all the shelters in the database.
+
+def registerPuppy():
+
+
